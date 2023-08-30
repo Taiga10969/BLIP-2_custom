5c2008c0fa63a8759f53b53a226afc8b992e4523
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-08-30 20:08:13 +0900

    Add files via upload

---
+++ blip-2_forward_test.py
@@ -0,0 +1,73 @@
+import torch
+from lavis.models import load_model_and_preprocess
+from PIL import Image
+from torchinfo import summary
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_vis_processor(vis_processor_output, save_name='image.png'):
+    data = vis_processor_output.cpu().numpy()
+    data = (data - data.min()) / (data.max() - data.min())
+    data = np.transpose(data[0], (1, 2, 0))
+    plt.imshow(data)
+    plt.axis('off')  # 軸を表示しない
+    plt.savefig(save_name)
+    plt.close()
+
+# check GPU usage
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.device_count()==0: print('Use 1 GPU')
+else: print(f'Use {torch.cuda.device_count()} GPUs')
+
+model_pth  ='/taiga/moonshot/BLIP2_custom/blip2_t5_pretrain_flant5xl_ALL.pth'
+
+# モデルの読み込み
+model, vis_processors, _ = load_model_and_preprocess(
+    name='blip2_t5_custom',
+    model_type='pretrain_flant5xl', #pretrain_flant5xl, caption_coco_flant5xl, pretrain_flant5xxl
+    is_eval=True,
+    #device=device
+)
+
+# model samarry
+summary(model)
+#print(dir(model))
+#print(vis_processors['eval'])
+
+
+# モデル内の階層名を表示
+for name, module in model.named_children():
+    print(name)
+
+model.load_state_dict(torch.load(model_pth, map_location=torch.device('cpu')))
+
+model.to(device)
+
+# マーライオンの画像 (論文の再現実験)===============================================================
+print('=====image1=====')
+# 画像の読み込み
+raw_image1 = Image.open('/taiga/moonshot/BLIP2_custom/images/merlion.png').convert('RGB')
+image1 = vis_processors['eval'](raw_image1).unsqueeze(0).to(device)
+#print('vis_processors output : ', image)
+print('vis_processors output.shape : ', image1.shape)
+
+prompt1 = "Question: which city is this? Answer:"
+answer1 = 'singapore'
+
+raw_image2 = Image.open('/taiga/moonshot/BLIP2_custom/images/flant5.png').convert('RGB')
+image2 = vis_processors['eval'](raw_image2).unsqueeze(0).to(device)
+
+prompt2 = "Question: what type of figure in this image? Answer:"
+answer2 = 'line graph'
+
+input_datas = {
+    "image": torch.cat([image1, image2], dim=0),
+    "text_input": [prompt1, prompt2],
+    "text_output": [answer1, answer2]
+}
+
+output = model.forward_test(input_datas)
+
+print('loss : ', output['loss'])
+#print('output[text] : ', output['text'])
+print('output[text][logits].shape : ', output['text']['logits'].shape)
