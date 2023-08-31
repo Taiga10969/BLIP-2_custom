a9b50a633405605ab6f49ee4d03af64ee243ca7a
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-08-31 19:55:02 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
 import torch
+import cv2
 import numpy as np
 
 
 
-def attention_rollout(attention_weight): 
+def attention_rollout(attention_weight, normalization_method='min-max'): 
     #input>attention_weight　np.shape() : (L,H,N,N)
     #L:層数, H:ヘッド数, N:パッチ数+cls_token
 
     #ヘッド方向に平均
-    attention_weight = np.mean(attention_weight, axis=1) # shape:(L,N,N)
+    mean_head = np.mean(attention_weight, axis=1) # shape:(L,N,N)
 
     #N*Nの単位行列を加算
     mean_head = mean_head + np.eye(mean_head.shape[1])
@@ -23,9 +24,28 @@ def attention_rollout(attention_weight):
 
     #クラストークンと各パッチトークン間っとのAttention Weightから，
     #入力画像サイズまで正規化しながらリサイズしてAttention Mapを生成
-    mask = v[0, 1:].reshape(14,14)
-    attention_map = cv2.resize(mask / mask.max(), (ori_img.shape[2], ori_img.shape[3]))[np.newaxis]
+    mask = v[0, 1:].reshape(16,16)
+    mask = normalize_attention_map(mask, normalization_method)
+    attention_map = cv2.resize(mask, (224, 224))[np.newaxis]
 
-    return attention_map
+    return attention_map[0]
   
   
+def normalize_attention_map(attention_map, normalization_method='min-max'):
+    if normalization_method == 'min-max':
+        min_attention = attention_map.min()
+        max_attention = attention_map.max()
+        normalized_attention = (attention_map - min_attention) / (max_attention - min_attention)
+    elif normalization_method == 'max':
+        normalized_attention = attention_map / attention_map.max()
+    elif normalization_method == 'z-score':
+        mean_attention = attention_map.mean()
+        std_attention = attention_map.std()
+        normalized_attention = (attention_map - mean_attention) / std_attention
+    elif normalization_method == 'softmax':
+        exp_attention = np.exp(attention_map)
+        normalized_attention = exp_attention / np.sum(exp_attention)
+    else:
+        raise ValueError("Invalid normalization method")
+
+    return normalized_attention
